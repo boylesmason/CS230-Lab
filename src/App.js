b53789761173a64454e299c9541d00d98fc38b07
boylesmason <mtb00011@mix.wvu.edu>
2024-02-25 21:39:47 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 002</h2>
+      <p>WVU ID: 800365945</p>
+      <p>Hi, I am Mason</p>
+    </div>
+  );
+}
+
+export default App;
