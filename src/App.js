a59ef53d5f1dc98741dd5be8031198eeabbc7526
boylesmason <mtb00011@mix.wvu.edu>
2024-03-03 20:44:24 -0500

    !

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <h1>CS 230L</h1>
       <h2>Section - 002</h2>
       <p>WVU ID: 800365945</p>
-      <p>Hi, I am Mason</p>
+      <p>Hi, I am Mason!</p>
       <Card />
     </div>
   );
